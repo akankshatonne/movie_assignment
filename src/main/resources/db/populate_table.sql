597e31688aaf9aad5d7a7076dc34401b809ad71b
aprakasht <akanksha_prakasht@intuit.com>
2024-12-05 14:27:45 +0530

    Added db setup sql script

--- src/main/resources/db/populate_table.sql
+++ src/main/resources/db/populate_table.sql
@@ -1,3 +1,66 @@
+create table if not exists public.director
+(
+    id          varchar(255) not null
+    primary key,
+    name        varchar(255),
+    nationality varchar(255)
+    );
+
+alter table public.director
+    owner to postgres;
+
+create table if not exists public.genre
+(
+    id   varchar(255) not null
+    primary key,
+    name varchar(255)
+    );
+
+alter table public.genre
+    owner to postgres;
+
+create table if not exists public.movie
+(
+    id           varchar(255) not null
+    primary key,
+    release_year integer,
+    title        varchar(255),
+    release_date date
+    );
+
+alter table public.movie
+    owner to postgres;
+
+create table if not exists public.movie_director
+(
+    director_id varchar(255) not null
+    constraint fkgn1rkfh7ioiax467kc9dcrcrr
+    references public.director,
+    movie_id    varchar(255) not null
+    constraint fkbay4b2v2db4yfaww2oocpld9m
+    references public.movie
+    );
+
+alter table public.movie_director
+    owner to postgres;
+
+create table if not exists public.movie_genre
+(
+    genre_id varchar(255) not null
+    constraint fk86p3roa187k12avqfl28klp1q
+    references public.genre,
+    movie_id varchar(255) not null
+    constraint fkp6vjabv2e2435at1hnuxg64yv
+    references public.movie
+    );
+
+alter table public.movie_genre
+    owner to postgres;
+
+
+
+
+
 INSERT INTO director (id, name, nationality) VALUES
                                                  ('D1', 'Christopher Nolan', 'British-American'),
                                                  ('D2', 'Steven Spielberg', 'American'),
